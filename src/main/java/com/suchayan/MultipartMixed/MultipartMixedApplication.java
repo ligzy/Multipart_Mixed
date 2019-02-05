7ccb4d46d5fbbdab15ba597a0e8c3d4a29378431
suchayanchatterjee <suchayanchatterjee@gmail.com>
2019-02-05 23:45:47 +0530

    Initial Commit

---
+++ src/main/java/com/suchayan/MultipartMixed/MultipartMixedApplication.java
@@ -0,0 +1,14 @@
+package com.suchayan.MultipartMixed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipartMixedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipartMixedApplication.class, args);
+	}
+
+}
+
